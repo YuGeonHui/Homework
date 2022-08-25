0d259201da3c596a5412217d45b456c98d7b42e9
YuGeonHui <ghyu_0906@naver.com>
2022-08-25 21:31:27 +0900

    ChatListCollectionVIew Model & View Dev

--- Homework6/ChatListCollectionViewCell.swift
+++ Homework6/ChatListCollectionViewCell.swift
@@ -9,4 +9,16 @@ import UIKit
 
 class ChatListCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var chatLabel: UILabel!
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    func config(_ chat: ChatModel) {
+        
+        thumbnailImageView.image = UIImage(named: chat.name)
+        nameLabel.text = chat.name
+        chatLabel.text = chat.chat
+        dataLabel.text = chat.date
+    }
 }
--- Homework6/ChatListViewController.swift
+++ Homework6/ChatListViewController.swift
@@ -8,22 +8,39 @@
 import UIKit
 
 class ChatListViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let chatList: [ChatModel] = ChatModel.list
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+    }
+}
+
+extension ChatListViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return chatList.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChatListCollectionViewCell", for: indexPath) as? ChatListCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
     }
-    */
+}
 
+extension ChatListViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 100)
+    }
 }
