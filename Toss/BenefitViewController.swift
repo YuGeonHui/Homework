b5d13b250f6188cb18c8dbe13043cf9fd2478494
YuGeonHui <ghyu_0906@naver.com>
2022-09-25 19:35:25 +0900

    Toss 혜택 뷰 디자인 및 데이터 개발완료

--- Toss/BenefitViewController.swift
+++ Toss/BenefitViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class BenefitViewController: UIViewController {
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     typealias Item = AnyHashable
@@ -21,7 +21,7 @@ class BenefitViewController: UIViewController {
     var datasource: UICollectionViewDiffableDataSource<Section, Item>!
     
     var todaySectionItem: [AnyHashable] = TodaySectionItem(point: .defualt, today: .today).sectionItems
-    var otherSectionItem: [AnyHashable] = [Benefit.others]
+    var otherSectionItem: [AnyHashable] = Benefit.others
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,9 +29,9 @@ class BenefitViewController: UIViewController {
         datasource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
             
             guard let section = Section(rawValue: indexPath.section) else { return nil }
-
+            
             let cell = self.configureCell(for: section, item: item, collectionView: collectionView, indexPath: indexPath)
-
+            
             return cell
         })
         
@@ -41,12 +41,13 @@ class BenefitViewController: UIViewController {
         snapShot.appendItems(otherSectionItem, toSection: .other)
         datasource.apply(snapShot)
         
-        // collectionView.collectionViewLayout = layout()
+        collectionView.collectionViewLayout = layout()
+        collectionView.delegate = self
         
         navigationItem.title = "혜택"
-
+        
     }
-
+    
     
     private func configureCell(for section: Section, item: Item, collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell? {
         
@@ -86,7 +87,30 @@ class BenefitViewController: UIViewController {
         }
     }
     
-//    private func layout() -> UICollectionViewCompositionalLayout {
-//
-//    }
+    private func layout() -> UICollectionViewCompositionalLayout {
+        
+        let spacing: CGFloat = 10
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(60))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(60))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        group.interItemSpacing = .fixed(10)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 16, bottom: 0, trailing: 16)
+        section.interGroupSpacing = spacing
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+}
+
+extension BenefitViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        let item = datasource.itemIdentifier(for: indexPath)
+        print("----> \(item)")
+    }
 }
